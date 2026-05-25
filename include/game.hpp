2dd1f2a46f199c97bd5bdda772c8e1c29d797144
Daniel <Danielrsanchez1990@gmail.com>
2026-05-24 19:43:42 -0700

    Game loops added for zones

--- include/game.hpp
+++ include/game.hpp
@@ -3,3 +3,6 @@
 void prompt();
 
 void north();
+void south();
+void east();
+void west();
--- src/game.cpp
+++ src/game.cpp
@@ -2,7 +2,15 @@
 
 #include "game.hpp"
 
+
+
+
 void prompt(void) {
+
+    bool gameRunning = true;
+
+    while (gameRunning) {
+
     std::cout << "You crashland on a planet and the scanner is broken." << std::endl;
     std::cout << "You need to find 3 parts to fix the ship and escape." << std::endl;
     std::cout << "Look around and you see there is a smoke coming from the north." << std::endl;
@@ -20,31 +28,167 @@ void prompt(void) {
     switch (choice) {
         case 1:
             north();
+            gameRunning = false;
             break;
         case 2:
-            std::cout << "You head south and find a dense forest." << std::endl;
+            south();
+            gameRunning = false;
             break;
         case 3:
-            std::cout << "You head east and find a river." << std::endl;
+            east();
+            gameRunning = false;
             break;
         case 4:
-            std::cout << "You head west and find a mountain." << std::endl;
-            break;
-        case 5:
-            std::cout << "You check the ship and find that the scanner is indeed broken." << std::endl;
+            west();
+            gameRunning = false;
             break;
         default:
             std::cout << "Invalid choice. Please try again." << std::endl;
             prompt();
             break;
+        }
     }
 
 }
 
+
 void north() {
+
+    bool isRunning = true;
+
     std::cout << "You head north and find a small village." << std::endl;
     std::cout << "The villagers look at you in awe." << std::endl;
     std::cout <<  "You try to ask them questions but they don't understand you" << std::endl;
     
-    std::cout << "You try to find a way to commiunicate so you look for a dirt and a stick to draw into the ground" << std::endl;
+    std::cout << "You try to find a way to commiunicate so you turn on your translator to see if it picks up any language" << std::endl;
+
+
+
+    while (isRunning) {
+
+        std::cout << "What should I do next?" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout <<  "Head to the shop for supplies with the money the villagers provided" << std::endl;
+                isRunning = false;
+                break;
+            case 2:
+                std::cout << "" << std::endl;
+                isRunning = false;
+                break;
+            default:
+                std::cout << "Invalid choice. Please choose again." << std::endl;
+                break;
+        }
+    }  
 }
+
+void south() {
+
+    bool isRunning = true;
+
+    std::cout << "You head north and find a small village." << std::endl;
+    std::cout << "The villagers look at you in awe." << std::endl;
+    std::cout <<  "You try to ask them questions but they don't understand you" << std::endl;
+    
+    std::cout << "You try to find a way to commiunicate so you turn on your translator to see if it picks up any language" << std::endl;
+
+
+
+    while (isRunning) {
+
+        std::cout << "What should I do next?" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout <<  "Head to the shop for supplies with the money the villagers provided" << std::endl;
+                isRunning = false;
+                break;
+            case 2:
+                std::cout << "" << std::endl;
+                isRunning = false;
+                break;
+            default:
+                std::cout << "Invalid choice. Please choose again." << std::endl;
+                break;
+        }
+    }  
+}
+
+void east() {
+
+    bool isRunning = true;
+
+    std::cout << "You head north and find a small village." << std::endl;
+    std::cout << "The villagers look at you in awe." << std::endl;
+    std::cout <<  "You try to ask them questions but they don't understand you" << std::endl;
+    
+    std::cout << "You try to find a way to commiunicate so you turn on your translator to see if it picks up any language" << std::endl;
+
+
+
+    while (isRunning) {
+
+        std::cout << "What should I do next?" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout <<  "Head to the shop for supplies with the money the villagers provided" << std::endl;
+                isRunning = false;
+                break;
+            case 2:
+                std::cout << "" << std::endl;
+                isRunning = false;
+                break;
+            default:
+                std::cout << "Invalid choice. Please choose again." << std::endl;
+                break;
+        }
+    }  
+}
+
+void west() {
+
+    bool isRunning = true;
+
+    std::cout << "You head north and find a small village." << std::endl;
+    std::cout << "The villagers look at you in awe." << std::endl;
+    std::cout <<  "You try to ask them questions but they don't understand you" << std::endl;
+    
+    std::cout << "You try to find a way to commiunicate so you turn on your translator to see if it picks up any language" << std::endl;
+
+
+
+    while (isRunning) {
+
+        std::cout << "What should I do next?" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout <<  "Head to the shop for supplies with the money the villagers provided" << std::endl;
+                isRunning = false;
+                break;
+            case 2:
+                std::cout << "" << std::endl;
+                isRunning = false;
+                break;
+            default:
+                std::cout << "Invalid choice. Please choose again." << std::endl;
+                break;
+        }
+    }  
+}
+
